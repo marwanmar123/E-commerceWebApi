775d69d262876e044f8ce70641e0cb3785ba7cec
marwanmar123 <youcode098@gmail.com>
2021-04-05 09:44:21 +0100

    add repostory

---
+++ AuthJWT/Repostory/IRepo.cs
@@ -0,0 +1,17 @@
+﻿using AuthJWT.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthJWT.Repostory
+{
+    public interface IRepo
+    {
+        Task<List<User>> GetUsers();
+        Task<User> GetUser(string id);
+        Task<User> AddUser(User user);
+        Task<int> DeleteUser(string id);
+        Task UpdateUser(User user);
+    }
+}
---
+++ AuthJWT/Repostory/UserRepo.cs
@@ -0,0 +1,50 @@
+﻿using AuthJWT.Data;
+using AuthJWT.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthJWT.Repostory
+{
+    public class UserRepo : IRepo
+    {
+        private readonly ApplicationDbContext _db;
+
+        public UserRepo(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public async Task<User> AddUser(User user)
+        {
+            await _db.Users.AddAsync(user);
+            await _db.SaveChangesAsync();
+            return user;
+        }
+
+        public async Task<int> DeleteUser(string id)
+        {
+            var user = await _db.Users.FirstOrDefaultAsync(i => i.Id == id);
+            _db.Users.Remove(user);
+            var save = await _db.SaveChangesAsync();
+            return save;
+        }
+
+        public async Task<User> GetUser(string id)
+        {
+            return await _db.Users.FindAsync(id);
+        }
+
+        public async Task<List<User>> GetUsers()
+        {
+            return await _db.Users.ToListAsync();
+        }
+
+        public async Task UpdateUser(User user)
+        {
+            _db.Users.Update(user);
+            await _db.SaveChangesAsync();
+        }
+    }
+}

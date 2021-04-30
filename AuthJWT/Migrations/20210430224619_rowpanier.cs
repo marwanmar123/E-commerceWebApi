8409b43ee81170530654a99776bdf3b9270e6796
marwanmar123 <youcode098@gmail.com>
2021-04-30 23:06:36 +0000

    panier Init

---
+++ AuthJWT/Migrations/20210430224619_rowpanier.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AuthJWT.Migrations
+{
+    public partial class rowpanier : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "RowPaniers",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    ProductId = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    Quantity = table.Column<double>(type: "float", nullable: false),
+                    PanierId = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RowPaniers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_RowPaniers_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RowPaniers_ProductId",
+                table: "RowPaniers",
+                column: "ProductId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RowPaniers");
+        }
+    }
+}
--- AuthJWT/Models/Panier.cs
+++ AuthJWT/Models/Panier.cs
@@ -1,13 +1,45 @@
-﻿using System;
+﻿using AuthJWT.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace AuthJWT.Models
 {
-    public class Panier
+    public class Panier : IDisposable
     {
-        public int Id { get; set; }
-        public int UserId { get; set; }
+        private readonly ApplicationDbContext _db;
+        private readonly string _panierId;
+
+        public Panier(ApplicationDbContext Db, string PanierId)
+        {
+            _db = Db;
+            _panierId = PanierId;
+        }
+
+        public void AddProduct(string Product_Id)
+        {
+            RowPanier rowPanier = _db.RowPaniers.SingleOrDefault(p => p.PanierId == _panierId && p.ProductId == Product_Id);
+            if (rowPanier == null)
+            {
+                rowPanier = new RowPanier
+                {
+                    PanierId = _panierId,
+                    ProductId = Product_Id,
+                    Quantity = 1
+                };
+                _db.RowPaniers.Add(rowPanier);
+            }
+            else
+            {
+                rowPanier.Quantity++;
+            }
+            _db.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
